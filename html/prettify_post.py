7d3d34681cd17bd4c1360870dc21372e6a6b1556
Reza Borhani <reza@Rezas-MacBook-Pro.local>
2019-04-09 16:46:07 -0500

    major reorg

--- html/prettify_post.py
+++ html/prettify_post.py
@@ -15,85 +15,89 @@ name = soup.html.head.title.string
 
 # change title to page_title
 soup.html.head.title.string = page_title
+
+RepoName='control-notes'
+
+series_url='foo'
  
 	
 # This script adds navigation bar + sharing logos + title
 script_1 = '''
 
-   <!-- Navigation -->
-    <nav class="navbar navbar-default navbar-custom navbar-fixed-top">
-        <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header page-scroll">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    Menu <i class="fa fa-bars"></i>
-                </button>
-                <a class="navbar-brand" href="https://dgsix.com">degree six</a>
-            </div>
+ <br><br><br>
 
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-                    <li>
-                        <a href="../../index.html">Homepage</a>
-                    </li>
-                    <li>
-                        <a href="../../subscribe.html">Subscribe</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
+<!-- share buttons -->
+<div style="width: 63%; margin:auto;">
 
+	<div id="1" style="width: 70%; float:left;">
+		<span style="color:black; font-family:'lato', sans-serif; font-size: 18px;">code</span>
+		<div style="width: 95px; border-bottom: solid 1px; border-color:black;">
 
-    <hr>
+			<div class="logo-share"></div>
+			<div class="logo-share"></div>
 
-<div class="page-title" style="text-align: center !important;"> 
-<!-- github
-<div><a href="https://github.com/jermwatt/machine_learning_refined" style="text-decoration: none" target="_blank"><button class="btn-star">★ Our Project On GitHub</button></a></div>
--->
-<div style="width: 100%; margin:auto;">
+			<div class="logo-share">
+				<!-- github -->
+				<a target="_blank" href="https://github.com/jermwatt/'''+RepoName+'''">
+					<img src="../../html/pics/github.png" width=28 height=28 onmouseover="this.src='../../html/pics/github_filled.png';" onmouseout="this.src='../../html/pics/github.png';">
+				</a>
+			</div>
+		</div>
+	</div>
 
-<div class="logo-share">
-<!-- linkedin -->
-<a href="https://www.linkedin.com/cws/share?url=https%3A%2F%2Fblog.dgsix.com%2F'''+ filename+ '''" target="_blank">
-<img height="18" onmouseout="this.src='../../html/images/linkedin_off.png';" onmouseover="this.src='../../html/images/linkedin_on.png';" src="../../html/images/linkedin_off.png" width="18"/>
-</a>
-</div>
+	<div id="2" style="width: 30%; float:left;">
+		<span style="color:black; font-family:'lato', sans-serif; font-size: 18px;">share</span>
+		<div style="width: 280px; border-bottom: solid 1px; border-color:black;">
 
+			<div class="logo-share"></div>
+			<div class="logo-share"></div>
 
-<div class="logo-share">
-<!-- facebook -->
-<a href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fblog.dgsix.com%2F'''+ filename+'''" target="_blank">
-<img height="18" onmouseout="this.src='../../html/images/facebook_off.png';" onmouseover="this.src='../../html/images/facebook_on.png';" src="../../html/images/facebook_off.png" width="18"/>
-</a>
-</div>
+			<div class="logo-share">
+				<!-- linkedin -->
+				<a target="_blank" href="https://www.linkedin.com/cws/share?url=https%3A%2F%2Fjermwatt.github.io%2F'''+RepoName+'''%2Fposts%2F'''+name+'''%2F'''+ name+'''.html">
+					<img src="../../html/pics/linkedin.png" width=28 height=28 onmouseover="this.src='../../html/pics/linkedin_filled.png';" onmouseout="this.src='../../html/pics/linkedin.png';">
+				</a>
+			</div>
 
+			<div class="logo-share"></div>
 
-<div class="logo-share">
-<!-- twitter -->
-<a href="https://twitter.com/intent/tweet?ref_src=twsrc%5Etfw&amp;tw_p=tweetbutton&amp;url=https%3A%2F%2Fblog.dgsix.com%2F'''+ filename+'''" target="_blank">
-<img height="18" onmouseout="this.src='../../html/images/twitter_off.png';" onmouseover="this.src='../../html/images/twitter_on.png';" src="../../html/images/twitter_off.png" width="18"/>
-</a>
-</div>
+			<div class="logo-share">
+				<!-- twitter -->
+				<a target="_blank" href="https://twitter.com/intent/tweet?ref_src=twsrc%5Etfw&tw_p=tweetbutton&url=https%3A%2F%2Fjermwatt.github.io%2F'''+RepoName+'''%2Fposts%2F'''+name+'''%2F'''+ name+'''.html">
+					<img src="../../html/pics/twitter.png" width=28 height=28 onmouseover="this.src='../../html/pics/twitter_filled.png';" onmouseout="this.src='../../html/pics/twitter.png';">
+				</a>
+			</div>
 
-<div class="logo-share">
-<!-- github -->
-<a href="https://github.com/jermwatt/dgsix_blog" target="_blank">
-<img height="18" onmouseout="this.src='../../html/images/github_off.png';" onmouseover="this.src='../../html/images/github_on.png';" src="../../html/images/github_off.png" width="18"/>
-</a>
+			<div class="logo-share"></div>
+
+			<div class="logo-share">
+				<!-- facebook -->
+				<a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fjermwatt.github.io%2F'''+RepoName+'''%2Fposts%2F'''+name+'''%2F'''+ name+'''.html">
+					<img src="../../html/pics/facebook.png" width=28 height=28 onmouseover="this.src='../../html/pics/facebook_filled.png';" onmouseout="this.src='../../html/pics/facebook.png';">
+				</a>
+			</div>
+			
+			<div class="logo-share"></div>
+
+			<div class="logo-share">
+				<!-- reddit -->
+				<a target="_blank" href="https://www.reddit.com/submit?url=https%3A%2F%2Fjermwatt.github.io%2F'''+RepoName+'''%2Fposts%2F'''+name+'''%2F'''+ name+'''.html">
+					<img src="../../html/pics/reddit.png" width=28 height=28 onmouseover="this.src='../../html/pics/reddit_filled.png';" onmouseout="this.src='../../html/pics/reddit.png';">
+				</a>
+			</div>
+			
+		</div>
+
+	</div>
 </div>
 
 <br><br>
-	<div style="padding: 0px;">'''+ page_title +'''</div>
-<svg width="80" height="15">
-  <rect width="80" height="15" style="fill:#0085A1; stroke-width:0; stroke:rgb(0,0,0)" /> 
-</svg>	
+<div class="page-title" style="text-align: center !important;">
+<div><a href="https://github.com/jermwatt/'''+RepoName+'''" style="text-decoration: none" target="_blank"><button class="btn-star">★ Our Project On GitHub</button></a></div>
+	<br><br>
+	<mark style="padding: 0px; background-color: #f9f3c2;">'''+ page_title +'''</mark>
 </div>
-'''
+<br>'''
 
 # parse script as BeautifulSoup object
 html_1 = BeautifulSoup(script_1,'html.parser')
@@ -103,43 +107,44 @@ soup.body.insert(0, html_1)
 
 
 # # This script adds comment section to the bottom of the page
-script_2 = '''
+#script_2 = '''
  
-   <!-- Footer -->
+#   <!-- Footer -->
 
  
  
  
- <br><br><br><br><br><br>
+# <br><br><br><br><br><br>
 
- <!-- comment section -->
- <div id="disqus_thread" style="width:70%; height:auto; margin:auto;"></div>
- <script>
- (function() { // DON'T EDIT BELOW THIS LINE
- var d = document, s = d.createElement('script');
- s.src = 'https://degreesix.disqus.com/embed.js';
- s.setAttribute('data-timestamp', +new Date());
- (d.head || d.body).appendChild(s);
- })();
- </script>
- <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
- '''
+# <!-- comment section -->
+# <div id="disqus_thread" style="width:70%; height:auto; margin:auto;"></div>
+# <script>
+# (function() { // DON'T EDIT BELOW THIS LINE
+# var d = document, s = d.createElement('script');
+# s.src = 'https://degreesix.disqus.com/embed.js';
+# s.setAttribute('data-timestamp', +new Date());
+# (d.head || d.body).appendChild(s);
+# })();
+# </script>
+# <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+# '''
 
 # # parse script as BeautifulSoup object
-html_2 = BeautifulSoup(script_2,'html.parser')
+#html_2 = BeautifulSoup(script_2,'html.parser')
 
 # # insert it as the last element of body tag, hence: -1
-soup.body.insert(len(soup.body.contents), html_2)
+#soup.body.insert(len(soup.body.contents), html_2)
+
+
+# meta tags
+# <meta property="og:title" content="'''+page_title+'''">
+# <meta property="og:image" content="https://raw.githubusercontent.com/jermwatt/machine_learning_refined/gh-pages/html/pics/meta.png">
+# <meta property="og:url" content="https://blog.dgsix.com/'''+filename+'''">
+# <meta name="twitter:card" content="summary_large_image">
 
 
 # This script changes default LateX font to a prettier version
-script_3 = '''
-
-	<meta property="og:title" content="'''+page_title+'''">
-	<meta property="og:image" content="https://raw.githubusercontent.com/jermwatt/machine_learning_refined/gh-pages/html/pics/meta.png">
-	<meta property="og:url" content="https://blog.dgsix.com/'''+filename+'''">
-	<meta name="twitter:card" content="summary_large_image">	
-
+script_3 = '''	
     <script type="text/x-mathjax-config">
     MathJax.Hub.Config({
     	TeX: { equationNumbers: { autoNumber: "AMS" } },
@@ -159,11 +164,9 @@ script_3 = '''
             linebreaks: { automatic: true }
         }
     });
-    </script>
+       </script>
 
     <link href="../../html/CSS/custom.css" rel="stylesheet"/>
-    <link href="../../html/CSS/mystyle.css" rel="stylesheet"/>
-    
 
     <style>
         p {
@@ -171,26 +174,6 @@ script_3 = '''
             text-justify: inter-word !important;
         }
     </style>
-    
-    
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Clean Blog - Sample Post</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="../../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Theme CSS -->
-    <link href="../../html/CSS/clean-blog.css" rel="stylesheet">
-
-    <!-- Custom Fonts -->
-    <link href="../../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
     '''
 
 # parse script as BeautifulSoup object
